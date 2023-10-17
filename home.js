b8e2e5d9338ca7a8a64bbd25d8ad8051b8f10605
Shanmukha P <79988783+shanmukhapolu@users.noreply.github.com>
2023-10-17 02:35:36 +0000

    Updated code

--- home.js
+++ home.js
@@ -17,3 +17,13 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('unsticky');
   }
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("nav").style.background = " #00000015";
+  } else {
+    document.getElementById("nav").style.background = " #00000000";
+  }
+}
